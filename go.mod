18ba43b8c7df17b2598538ae82328b0b311acaca
digitalyoshixi <digitalyoshixi@proton.me>
2025-02-15 10:48:17 -0500

    able to access web pages

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gemini
 
 go 1.23.6
+
+require (
+	git.sr.ht/~adnano/go-gemini v0.2.6 // indirect
+	github.com/reiver/go-gemini v0.0.0-20211012025811-64df1daf7b5e // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"git.sr.ht/~adnano/go-gemini"
+	"fmt"
+	"context"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
-
-
+	client := &gemini.Client{}
+	ctx := context.Background()
+	resp, err := client.Get(ctx, "gemini://geminiprotocol.net/")
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+  fmt.Println(resp.Status)
+  fmt.Println(resp.Meta)
+  fmt.Println(resp.Body)
+	defer resp.Body.Close()
 }
