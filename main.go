ba9c6c8fdb623e16494981a33d24f34022bb1e36
digitalyoshixi <digitalyoshixi@proton.me>
2025-02-15 11:00:47 -0500

    able to parse the contents of gemini webpage

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"git.sr.ht/~adnano/go-gemini"
 	"fmt"
 	"context"
+	"io"
+	"log"
 )
 
 func main() {
@@ -11,11 +13,21 @@ func main() {
 	ctx := context.Background()
 	resp, err := client.Get(ctx, "gemini://geminiprotocol.net/")
 	if err != nil {
-		// handle error
 		fmt.Println(err)
+		return
 	}
-  fmt.Println(resp.Status)
-  fmt.Println(resp.Meta)
-  fmt.Println(resp.Body)
 	defer resp.Body.Close()
+
+	fmt.Println(resp.Status) // This will print the status code of the response
+	fmt.Println(resp.Meta) // This will print the status code of the response
+
+  // Read the body
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  // Use the body contents (it's a []byte)
+  fmt.Println(string(body))
+
 }
